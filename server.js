64fc7f32063c79b4eda64c859e8c546fd009df41
Lucia Brammer <brammerl96@gmail.com>
2020-05-07 17:57:10 -0700

    only selecting all of the data

--- server.js
+++ server.js
@@ -9,17 +9,13 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
-app.get('/snakes', async(req, res) => {
+app.get('/api/snakes', async(req, res) => {
   const data = await client.query('select * from snakes');
 
   res.json(data.rows);
 });
 
-app.get('/snakeNames', async(req, res) => {
-  const data = await client.query('select species from snakes');
 
-  res.json(data.rows);
-});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
